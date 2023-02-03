b9d7d51d9c4faa8fb2aa6a587e455fde19f8765b
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-02-03 12:38:42 -0800

    Started api calls and submit functions

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 var weatherAPI = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={6e9212c1940c5264a43ed792ce5f5c11}';
 var geocoderAPI = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={6e9212c1940c5264a43ed792ce5f5c11}';
 
+var seachInput = document.getElementById('citySearch');
+var searchBtn = document.getElementsByClassName('search');
 
+function getLocation() {
+    
+}
+function getWeather() {  
 fetch(weatherAPI) 
     .then(function (response) {
-        console.log(response.status);
+        return response.json();
+        // console.log(response.status);
     });
+};
