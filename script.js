483d910ff53273bb54f8c818a10f4ed343cbbc5c
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-02-05 13:22:33 -0800

    created and appended date to header

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-var weatherAPI = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={6e9212c1940c5264a43ed792ce5f5c11}';
-var geocoderAPI = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={6e9212c1940c5264a43ed792ce5f5c11}';
+let APIKey = '6e9212c1940c5264a43ed792ce5f5c11';
+var weatherAPI = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=' + APIKey;
+var geocoderAPI = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit={limit}&appid=' + APIKey;
 
-var searchBtn = $('#search');
-
+var headerDate = $('.date');
+var citySearch = $("#citySeach");
+var searchCardEL = $('.card');
 
 var createDate = function() {  
-    var headerDate = document.getElementsByClassName('date');
-    var currrentDate = dayjs();
-    currrentDate.format('dddd, MMM, YYYY');
+    var currrentDate = dayjs().format('dddd, DD MMM YYYY');
     console.log(currrentDate);
+    headerDate.text(currrentDate);
 };
 
-searchBtn.on('submit', function(event) {
+ citySearch.on('submit', function(event) {
     event.preventDefault();
-    var seachInput = $('input[name="citySearch"]').val();
+    var city = $('city').val();
+    localStorage.setItem('Cities', JSON.stringify(city));
 
-    // if (!seachInput) {
-
-    // }
-   
-    localStorage.setItem('searchInput', JSON.stringify('searchInput'));
-
-});
+   })
 
 
 createDate();
