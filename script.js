067a44b34571335ede052526d2be4cf889784a15
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-02-09 22:41:22 -0800

    changed display errors

--- script.js
+++ script.js
@@ -46,12 +46,11 @@ console.log(data)
 var cardText = `<div class="card" style="width: 18rem;">
 <div class="card-body">
   <h5 class="card-title"></h5>
-  <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+  <h6 class="card-subtitle mb-2 text-muted">${currrentDate}</h6>
   <p class="card-text">Temp: ${data.main.temp}.</p>
   <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />
   <p>Conditions: ${data.weather[0].description}</p>
-  <a href="#" class="card-link">Card link</a>
-  <a href="#" class="card-link">Another link</a>
+
 </div>
 </div>
 `
