29eda4c8f84a81ee6e343bf0be34f1c1095a1346
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-04-29 12:10:16 -0700

    added call for forcast and removed createCards

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,65 @@
-let APIKey = '6e9212c1940c5264a43ed792ce5f5c11';
+let APIKey = "6e9212c1940c5264a43ed792ce5f5c11";
 
-
-var headerDate = $('.date');
+var headerDate = $(".date");
 var citySearch = $("#citySearch");
-var searchCardEL = $('.card');
+var searchCardEL = $(".card");
 
-
-
-var createDate = function() {  
-    var currrentDate = dayjs().format('dddd, DD MMM YYYY');
-    console.log(currrentDate);
-    headerDate.text(currrentDate);
+var createDate = function () {
+  var currrentDate = dayjs().format("dddd, DD MMM YYYY");
+  console.log(currrentDate);
+  headerDate.text(currrentDate);
 };
 
-var getCityWeather = function(city) {
-    createCard();
+var getCityWeather = function (city) {
+  var weatherAPI =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey +
+    "&units=imperial";
+  fetch(weatherAPI)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      createWeatherCard(data);
+    });
 
-    var weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q=' + city +'&appid=' + APIKey+"&units=imperial";
-    fetch(weatherAPI)
-        .then((response) => response.json())
-        .then((data) => {
-            
-            createWeatherCard(data);
-        });
-    
+    var forcast = 
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=" +
+    APIKey +
+    "&units=imperial";
+    fetch(forcast)
+    .then((response => response.json))
+    .then((data) => {
+        createWeatherCard(data);
+    })
 };
 
+citySearch.on("submit", function (event) {
+  event.preventDefault();
 
-citySearch.on('submit', function(event) {
-    event.preventDefault();
+  var city = $("#city").val();
+  localStorage.setItem("Cities", JSON.stringify(city));
+  city = city.toLowerCase();
+  getCityWeather(city);
 
-    var city = $('#city').val();
-    localStorage.setItem('Cities', JSON.stringify(city));
-    city = city.toLowerCase();
-    getCityWeather(city);
+  console.log(city);
+});
 
-    console.log(city);
-   })
+var createWeatherCard = function (data) {
+  console.log(data);
 
-   var createCard = function() {
-       for (var i = 0; i < 5; i++) {
-            var newCard = document.createElement('div');
-            newCard.classList.add('card');
-            document.getElementById('display').appendChild(newCard);
-       }
-   }
+  const fiveDayForcast = [
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+  ];
 
-var createWeatherCard = function(data) { 
-console.log(data)
-
-var cardText = `<div class="card" style="width: 18rem;">
+  var cardText = `<div class="card" style="width: 18rem;">
 <div class="card-body">
   <h5 class="card-title"></h5>
   <h6 class="card-subtitle mb-2 text-muted">Today</h6>
@@ -59,12 +69,8 @@ var cardText = `<div class="card" style="width: 18rem;">
 
 </div>
 </div>
-`
-        $(".card").append(cardText);
-   
- };
-
+`;
+  $(".card").append(cardText);
+};
 
 createDate();
-
-
